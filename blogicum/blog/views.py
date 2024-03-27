b6a9823f1047b36444da933980a9b382533b1ef1
Maria Kerimova <xfatcorex@gmail.com>
2024-03-28 01:47:13 +0300

    review_2, delete unused import

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
 from .models import Category, Post
